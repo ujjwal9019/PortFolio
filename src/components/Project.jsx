8878b349a324b5369f3f754405e5cac6d380b595
ujjwal9019 <ujjwal9019@gmail.com>
2024-03-10 00:44:12 +0530

    Bug Fixed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -36,17 +36,7 @@ function Project(props) {
     <p class="mt-3 text-sm text-gray-600">
      {props.description}
     </p>
-    <div class="mt-4">
-      <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-        #Macbook
-      </span>
-      <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-        #Apple
-      </span>
-      <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-        #Laptop
-      </span>
-    </div>
+   
     <button
       type="button"
       class="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
