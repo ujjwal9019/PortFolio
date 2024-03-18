e570828c63f425d26e7d004e7705c9bb7d061ee7
ujjwal9019 <ujjwal9019@gmail.com>
2024-03-18 23:55:12 +0530

    Resume updated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,7 @@ const Resume= ({ pdfUrl }) => {
       <div className="pdf-viewer ">
         <iframe
           title="PDF Viewer"
-          src= "/Ujjwal_Saxena_Resume.pdf"
+          src= "/Ujjwal_Saxena_Resume .pdf"
           width="100%"
           height="600px"
           frameBorder="0"
