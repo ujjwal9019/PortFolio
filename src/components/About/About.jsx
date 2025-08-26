4d29f07128e4dfc17586e56c983b873dff26987d
ujjwal9019 <ujjwal9019@gmail.com>
2025-08-26 16:05:09 +0530

    Resume Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,8 +16,7 @@ export default function About() {
                         </h2>
                         <p className="mt-6 text-gray-600">
                          
-
-                        Hello, I'm Ujjwal Saxena, a dedicated professional with a Bachelor's degree in Technology from GLA University, Mathura. My journey in the tech industry began with a rewarding experience as a Full Stack Developer at Fincrif, where I contributed my skills and knowledge for seven months. Now, I am eagerly seeking new opportunities to specialize in Product Management, focusing on delivering user-centric and impactful solutions. My passion for bridging technical expertise with business strategy, combined with my strong foundation in technology, makes me poised for success in the dynamic world of product development. I am excited about the prospect of contributing my skills to innovative projects and further enhancing my expertise in product management.
+                        Hello, I'm Ujjwal Saxena, a dedicated professional with a Bachelor's degree in Technology from GLA University, Mathura. I am currently working at IHMCL (Indian Highways Management Company Limited) as an Engineer, where I focus on electronic toll collection systems, FASTag data analysis, and product enhancement initiatives. With a strong foundation in technology and a passion for bridging technical expertise with business strategy, I am now seeking opportunities to specialize in Product Management, delivering user-centric and impactful solutions. I am excited to contribute my skills to innovative projects and further strengthen my expertise in product development and management..
                         </p>
                     </div>
                 </div>
